b70cc97d7f2565de0d453e639fd4e178966ea817
Deepak <deepakworkpc@gmail.com>
2024-09-03 22:43:17 +0530

    inital commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a href="/">home</a>
+    <a href="/demo">demo</a>
+</nav>
+
+<slot></slot>
---
+++ src/routes/demo/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    let data;
+  
+    onMount(async () => {
+      const response = await fetch('https://catfact.ninja/fact');
+      if (response.ok) {
+        data = await response.json();
+      } else {
+        console.error('Failed to fetch data');
+      }
+    });
+  </script>
+  
+  <main>
+    <h1>Fetched Data (Client-Side)</h1>
+    {#if data}
+      <p>{data.fact}</p>
+    {:else}
+      <p>Loading...</p>
+    {/if}
+  </main>
+  
+<!-- <script context="module">
+  export async function load() {
+    let response = await fetch("https://catfact.ninja/fact");
+    let data = await response.json()
+
+    console.log(data)
+
+    return {
+      props: {
+        message: "This is loaded data!",
+      },
+    };
+  }
+</script>
+
+<h1>Welcome to Demo</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p> -->
